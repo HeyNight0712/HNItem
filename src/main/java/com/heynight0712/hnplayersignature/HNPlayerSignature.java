5263b9f13997c29a5734ba5b665713fd0888b672
blacknight0712 <blacknight0981@gmail.com>
2024-04-01 09:52:16 +0800

    初始提交

---
+++ src/main/java/com/heynight0712/hnplayersignature/HNPlayerSignature.java
@@ -0,0 +1,17 @@
+package com.heynight0712.hnplayersignature;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNPlayerSignature extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
