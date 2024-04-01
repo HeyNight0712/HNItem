6bfbaaaa822153b4bd0e46d7a4ca40e3b2e89028
blacknight0712 <blacknight0981@gmail.com>
2024-04-01 10:15:25 +0800

    Add LanguageManager

--- src/main/java/com/heynight0712/hnplayersignature/HNPlayerSignature.java
+++ src/main/java/com/heynight0712/hnplayersignature/HNPlayerSignature.java
@@ -1,5 +1,6 @@
 package com.heynight0712.hnplayersignature;
 
+import com.heynight0712.hnplayersignature.core.LanguageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HNPlayerSignature extends JavaPlugin {
@@ -7,6 +8,8 @@ public final class HNPlayerSignature extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        LanguageManager.saveDefaultConfig(this);
+
         // Plugin startup logic
 
     }
---
+++ src/main/java/com/heynight0712/hnplayersignature/core/LanguageManager.java
@@ -0,0 +1,37 @@
+package com.heynight0712.hnplayersignature.core;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public class LanguageManager {
+    private static File languageFile;
+    private static FileConfiguration languageConfig;
+
+    public static void saveDefaultConfig(JavaPlugin plugin) {
+        if (languageFile == null) {
+            languageFile = new File(plugin.getDataFolder(), "language.yml");
+            if (!languageFile.exists()) {
+                languageFile.getParentFile().mkdirs();
+                plugin.saveResource("language.yml", false);
+            }
+        }
+
+        if (languageConfig == null) {
+            languageConfig = YamlConfiguration.loadConfiguration(languageFile);
+        }
+    }
+
+    public static FileConfiguration getConfig() {
+        if (languageConfig == null) {
+            throw new IllegalStateException("無法初始化,你有呼叫過 saveDefaultLanguageConfig?");
+        }
+        return languageConfig;
+    }
+
+    public static void reloadConfig(JavaPlugin plugin) {
+        languageConfig = YamlConfiguration.loadConfiguration(languageFile);
+    }
+}
